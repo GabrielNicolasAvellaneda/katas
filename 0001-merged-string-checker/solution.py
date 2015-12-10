6ac9ea3e4bcb72326e32b906cd83532c18e0e7d3
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-09 22:35:05 -0300

    Refactored to be compatible with CodeWars
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- 0001-merged-string-checker/solution.py
+++ 0001-merged-string-checker/solution.py
@@ -6,13 +6,13 @@ def are_all_empty(str, part1, part2):
 def parts_does_not_sum_up_to_string_size(str, part1, part2):
     return (len(part1) + len(part2) - len(str)) != 0
 
-def mergedstringchecker(str, part1, part2):
+def is_merge(str, part1, part2):
     if parts_does_not_sum_up_to_string_size(str, part1, part2):
         return False
 
-    return mergedstringchecker2(str, part1, part2)
+    return is_merge2(str, part1, part2)
 
-def mergedstringchecker2(str, part1, part2):
+def is_merge2(str, part1, part2):
     if are_all_empty(str, part1, part2):
         return True
 
@@ -21,43 +21,46 @@ def mergedstringchecker2(str, part1, part2):
 
     if len(part1) > 0 and len(part2) > 0 and part1[0] == target_char and part2[0] == target_char:
         # This is the special case
-        return mergedstringchecker2(target_substring, part1[1:], part2) or mergedstringchecker2(target_substring, part1, part2[1:])
-
-    if len(part1) > 0 and target_char == part1[0]:
-        return mergedstringchecker2(target_substring, part1[1:], part2)
+        part1_substring = part1[1:]
+        part2_substring = part2[1:]
+        return is_merge2(target_substring, part1_substring, part2) or is_merge2(target_substring, part1, part2_substring)
+    elif len(part1) > 0 and target_char == part1[0]:
+        part1_substring = part1[1:] 
+        return is_merge2(target_substring, part1_substring, part2)
     elif len(part2) > 0 and target_char == part2[0]:
-        return mergedstringchecker2(target_substring, part1, part2[1:])
+        part2_substring = part2[1:]
+        return is_merge2(target_substring, part1, part2_substring)
         
     return False
    
 class MergedStringCheckerTest(unittest.TestCase):
 
     def test_when_all_strings_are_empty_it_should_return_true(self):
-        result = mergedstringchecker("", "", "")
+        result = is_merge("", "", "")
         self.assertTrue(result)
 
     def test_with_string_bigger_than_sum_of_parts(self):
-        result = mergedstringchecker("abc", "a", "b")
+        result = is_merge("abc", "a", "b")
         self.assertFalse(result)
 
     def test_with_string_smaller_than_sum_of_parts(self):
-        result = mergedstringchecker("a","abc", "b")
+        result = is_merge("a","abc", "b")
         self.assertFalse(result)
 
     def test_with_string_and_parts_matching(self):
-        result = mergedstringchecker("a", "a", "")
+        result = is_merge("a", "a", "")
         self.assertTrue(result)
-        result = mergedstringchecker("some string", "", "some string")
+        result = is_merge("some string", "", "some string")
         self.assertTrue(result)
-        result = mergedstringchecker("code peace", "cd pc", "oeeae")
+        result = is_merge("code peace", "cd pc", "oeeae")
         self.assertTrue(result)
 
     def test_with_string_and_parts_not_matching(self):
-        result = mergedstringchecker("x", "y", "z")
+        result = is_merge("x", "y", "z")
         self.assertFalse(result)
 
     def test_with_parts_with_ambiguous_characters(self):
-        result = mergedstringchecker("oaob","ob", "oa")
+        result = is_merge("oaob","ob", "oa")
         self.assertTrue(result)
 
 if __name__ == "__main__":
