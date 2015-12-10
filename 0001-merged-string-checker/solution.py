e4b7b70c324572f52f955048f0093224cd43d65f
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-09 21:27:25 -0300

    Some base cases
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- 0001-merged-string-checker/solution.py
+++ 0001-merged-string-checker/solution.py
@@ -1,14 +1,31 @@
 import unittest
 
-def mergedstringchecker(str, part1, part2):
-    return True
+def are_all_empty(str, part1, part2):
+    return str == "" and part1 == "" and part2 == ""
 
+def string_empty_and_parts_non_empty(str, part1, part2):
+    return str == "" and (part1 != "" or part2 != "")
+
+def mergedstringchecker(str, part1, part2):
+    if are_all_empty(str, part1, part2):
+        return True  
+
+    if string_empty_and_parts_non_empty(str, part1, part2):
+        return False
 
 class MergedStringCheckerTest(unittest.TestCase):
 
     def test_when_all_strings_are_empty_it_should_return_true(self):
         result = mergedstringchecker("", "", "")
         self.assertTrue(result)
+
+    def test_string_empty_and_parts_not_empty(self):
+        result = mergedstringchecker("", "something", "")
+        self.assertFalse(result)
+        
+        result = mergedstringchecker("", "", "some text")
+        self.assertFalse(result)
+
 
 if __name__ == "__main__":
     unittest.main()
