00faf6061d7c145587c90a0c968a20270e5fdafc
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-10 11:53:28 -0300

    Decode a more phrase
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- 0002-decode-the-morse-code/solution.py
+++ 0002-decode-the-morse-code/solution.py
@@ -7,6 +7,7 @@ def decode_char(coded_char):
         "-..."  : "B",
         "-.-."  : "C",
         "."     : "E",
+        "-.."   : "D",
         "..-."  : "F",
         "--."   : "G",
         "...."  : "H",
@@ -25,20 +26,28 @@ def decode_char(coded_char):
         "..-"   : "U",
         "...-"  : "V",
         ".--"   : "W",
-        "-.."   : "X",
+        "-..-"   : "X",
         "-.--"  : "Y",
         "--.."  : "Z"
         }
     return MORSE_CODE_TABLE[coded_char]
 
+def decode_morse_word(morse_word):
+    output = []
+    char_separator = " "
+    for c in morse_word.split(char_separator):
+        char = decode_char(c)
+        output.append(char)
+    return string.join(output, "")
+
 """ https://en.wikipedia.org/wiki/Morse_code """
 def decode_morse(morse_code):
     output = []
-    char_separator = " "
-    for c in morse_code.split(char_separator):
-        char = decode_char(c)
-        output.append(char)
-    return string.join(output, "") 
+    word_separator = "   "
+    for w in morse_code.split(word_separator):
+        decoded_word = decode_morse_word(w)
+        output.append(decoded_word)
+    return string.join(output, " ") 
 
 class DecodeMorseCodeTest(unittest.TestCase):
 
@@ -51,6 +60,12 @@ class DecodeMorseCodeTest(unittest.TestCase):
     def test_morse_code_decoding_a_word(self):
         result = decode_morse(".... . -.--")
         self.assertEqual("HEY", result) 
+        result = decode_morse("... --- ...")
+        self.assertEqual("SOS", result)
+
+    def test_morse_code_decoding_multiple_words(self):
+        result = decode_morse(".... . -.--   -.. ..- -.. .")
+        self.assertEqual("HEY DUDE", result) 
 
 if __name__ == "__main__":
     unittest.main()
