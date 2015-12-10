d087b3c20d4e544bb8812ba5e987b3387acf3f12
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-09 21:46:48 -0300

    Fast fail on sum of parts not equal string size
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- 0001-merged-string-checker/solution.py
+++ 0001-merged-string-checker/solution.py
@@ -6,13 +6,21 @@ def are_all_empty(str, part1, part2):
 def string_empty_and_parts_non_empty(str, part1, part2):
     return str == "" and (part1 != "" or part2 != "")
 
+def parts_does_not_sum_up_to_string_size(str, part1, part2):
+    return (len(part1) + len(part2) - len(str)) != 0
+
 def mergedstringchecker(str, part1, part2):
+    if parts_does_not_sum_up_to_string_size(str, part1, part2):
+        return False
+
     if are_all_empty(str, part1, part2):
-        return True  
+        return True
 
     if string_empty_and_parts_non_empty(str, part1, part2):
         return False
 
+    return True
+   
 class MergedStringCheckerTest(unittest.TestCase):
 
     def test_when_all_strings_are_empty_it_should_return_true(self):
@@ -24,6 +32,10 @@ class MergedStringCheckerTest(unittest.TestCase):
         self.assertFalse(result)
         
         result = mergedstringchecker("", "", "some text")
+        self.assertFalse(result)
+
+    def test_with_string_bigger_than_sum_of_parts(self):
+        result = mergedstringchecker("abc", "a", "b")
         self.assertFalse(result)
 
 
