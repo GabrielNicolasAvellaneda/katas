d16eccfa59a3262b939e4fc44e8ca89c674d628d
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-11 14:25:54 -0300

    First iteration
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

---
+++ 0002-decode-the-morse-code/decode-morse-code/src/main/scala/MorseCodeDecoder.scala
@@ -0,0 +1,9 @@
+package com.kata
+/**
+ * Created by developer on 11/12/2015.
+ */
+object MorseCodeDecoder {
+  def decode(str: String) : String = {
+    "A"
+  }
+}
---
+++ 0002-decode-the-morse-code/decode-morse-code/src/test/scala/MorseCodeDecoderSpec.scala
@@ -0,0 +1,13 @@
+import com.kata.MorseCodeDecoder
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by developer on 11/12/2015.
+ */
+class MorseCodeDecoderSpec extends FlatSpec with Matchers {
+
+  "MorseCodeDecoder" should "decode a single character" in {
+
+    MorseCodeDecoder.decode(".") should === ("E")
+  }
+}
